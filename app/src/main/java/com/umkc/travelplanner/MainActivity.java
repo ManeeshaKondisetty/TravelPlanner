916387a2480d306e7fc8b124045b4bcbf4d378db
ManeeshaKondisetty <mpk5fd@umsystem.edu>
2021-04-16 00:16:14 -0500

    created hotel activity

--- app/src/main/java/com/umkc/travelplanner/MainActivity.java
+++ app/src/main/java/com/umkc/travelplanner/MainActivity.java
@@ -3,14 +3,16 @@ package com.umkc.travelplanner;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    public MainActivity() {
+        super(R.layout.activity_main);
     }
+
+
 }
