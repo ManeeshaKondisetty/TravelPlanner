6e64f146f694c9f41d1ce6f13274d7c4695c634f
ManeeshaKondisetty <mpk5fd@umsystem.edu>
2021-04-15 23:20:50 -0500

    added bottom bar

--- app/src/main/java/com/umkc/travelplanner/MainActivity.java
+++ app/src/main/java/com/umkc/travelplanner/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
